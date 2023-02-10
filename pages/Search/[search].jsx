b711cc526f151699187fbc372ee5db83a4d423ae
aliashraf123 <aliashrafali770@gmail.com>
2023-02-10 16:19:03 +0200

    sasad

--- pages/Search/[search].jsx
+++ pages/Search/[search].jsx
@@ -17,7 +17,7 @@ function Search() {
     if( !loading && data.total_pages == 0 ) {
         return (
             <ResultsControl resultstate={resultstate} setResultsState={setResultsState}>
-                <h1>Error...</h1>
+                <h1 className="w-full">There are no movies that matched your query.</h1>
              </ResultsControl>
         )
     }
@@ -33,8 +33,8 @@ function Search() {
                  <div className=" w-full px-10 flex flex-col gap-2 my-16 ">
                     {data.results.map(result =>{
                         return(
-                            <Link key={result.id} href={`/Movies/${result.id}`} className='flex flex-row  bg-[#121212] rounded-lg hover:scale-105 transform' passHref>
-                            {result.poster_path ? <Image width={200} className='w-[100px] h-full object-cover rounded-l-lg' height={300} src={`https://www.themoviedb.org/t/p/w220_and_h330_face${result.poster_path}`} alt={result.original_title} /> :  <Image width={200}  className='bg-slate-400 w-[100px] h-[150px] rounded-l-lg' height={300} src='https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg' alt={result.original_title} />}
+                            <Link key={result.id} href={`/Movies/${result.id}`} className='flex sm:flex-row flex-col    bg-[#121212] rounded-lg hover:scale-105 transform' passHref>
+                            {result.poster_path ? <Image width={200} className='w-[100px] h-full object-cover sm:rounded-l-lg m-auto' height={300} src={`https://www.themoviedb.org/t/p/w220_and_h330_face${result.poster_path}`} alt={result.original_title} /> :  <Image width={200}  className='bg-slate-400 w-[100px] h-[150px] rounded-l-lg' height={300} src='https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg' alt={result.original_title} />}
                                 <div className="m-6">
                                     <h3 className="text-1xl font-bold">{result.original_title || result.name}</h3>
                                     <p className="text-gray-300 mb-2">{result.release_date || result.first_air_date}</p>
