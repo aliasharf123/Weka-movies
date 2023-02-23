b661ace889a10536b0818332e3e2e29a92f30666
aliashraf123 <aliashrafali770@gmail.com>
2023-02-23 23:06:52 +0200

    ssssssssssssss

--- components/Results.jsx
+++ components/Results.jsx
@@ -11,8 +11,14 @@ function ResultsControl({children , resultstate , setResultsState ,setPage}) {
             <div className="flex flex-col gap-10 border-b border-[rgba(255,255,255,0.2)] ">
                 <h1 className="text-3xl text-center text-[rgba(255,255,255,0.8)]  font-medium">Search results: “{search}”</h1>
                 <ul className="flex  font-medium text-center text-[rgba(255,255,255,0.8)] m-auto gap-5  ">
-                    <li><button onClick={() =>setResultsState('movie')} className={` border-solid  px-2  pb-4 border-[#F4181C] ${resultstate =='movie' && 'border-b-2 text-white'}`}>Movies</button></li>
-                    <li><button onClick={() =>setResultsState('tv')} className={` border-solid  px-2  pb-4 border-[#F4181C] ${resultstate !=='movie' && 'border-b-2 text-white'}`}>Tv show</button></li>
+                    <li><button onClick={() =>{
+                        setResultsState('movie') 
+                        setPage(1)
+                        }} className={` border-solid  px-2  pb-4 border-[#F4181C] ${resultstate =='movie' && 'border-b-2 text-white'}`}>Movies</button></li>
+                    <li><button onClick={() =>{
+                        setResultsState('tv') 
+                        setPage(1)
+                        }} className={` border-solid  px-2  pb-4 border-[#F4181C] ${resultstate !=='movie' && 'border-b-2 text-white'}`}>Tv show</button></li>
                 </ul>
             </div>
             {children}
